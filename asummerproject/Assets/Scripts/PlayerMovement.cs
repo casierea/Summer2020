9889876162be25ef844ec8e22f18298d4fcf8d1d
Unknown <dancingmongoose@gmail.com>
2020-06-22 12:44:35 -0600

    unity movement

---
+++ asummerproject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{[SerializeField
+    ]
+    private float speed;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //new vector3 (1, 0, 0) *5 * realtime. 5* 1 =5, 5*0 =,0, 5*0=0 (5,0,0)
+        //transform.Translate(Vector3.right * speed * Time.deltaTime); practice
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horizontalInput,0,0) * speed * Time.deltaTime);
+    }
+}
