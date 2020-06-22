4efbea32d2c4c7bd05ef29271f1eba3c97281f51
Unknown <dancingmongoose@gmail.com>
2020-06-22 12:23:09 -0600

    Unity User input tutorial 1

---
+++ asummerproject/Assets/Scripts/StartPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPosition : MonoBehaviour
+{
+    public Vector3 startPlace;
+    void Start()
+    {
+        transform.position = startPlace;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
